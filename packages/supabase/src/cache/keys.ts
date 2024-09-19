cf8165eae5b110a9197f67ce906cf88d2946118a
Mostafa F <mostafa.foruji@gmail.com>
2024-09-19 20:53:56 +0330

    cache-keys-wip

---
+++ packages/supabase/src/cache/keys.ts
@@ -0,0 +1,55 @@
+export const accountKeys = {
+  list: () => ["accounts"] as const,
+  userAccounts: (user_id?: string) => ["user", user_id, "accounts"] as const,
+};
+
+export const invitationKeys = {
+  organizationInvites: (organization_id?: string) =>
+    ["organization", organization_id, "invitations"] as const,
+  teamInvites: (team_id?: string) => ["team", team_id, "invitations"] as const,
+  projectInvites: (project_id?: string) =>
+    ["project", project_id, "invitations"] as const,
+  userInvites: (user_id?: string) => ["user", user_id, "invitations"] as const,
+};
+
+export const navigationKeys = {
+  list: (account_name?: string, partial_name?: string) =>
+    ["account", account_name, partial_name, "navigations"] as const,
+  navigation: (
+    account_name?: string,
+    partial_name?: string,
+    nav_name?: string,
+  ) => [...navigationKeys.list(account_name, partial_name), nav_name] as const,
+};
+
+export const organizationKeys = {
+  list: () => ["organizations"] as const,
+  organization: (organization_id?: string) =>
+    ["organization", organization_id] as const,
+  members: (organization_id?: string) =>
+    ["organization", organization_id, "members"] as const,
+  userOrganizations: (user_id?: string) =>
+    ["user", user_id, "organizations"] as const,
+  membership: (organization_id?: string, user_id?: string) =>
+    [...organizationKeys.userOrganizations(user_id), organization_id] as const,
+};
+
+export const projectKeys = {
+  list: () => ["projects"] as const,
+  project: (project_id?: string) => ["project", project_id] as const,
+  members: (project_id?: string) => ["project", project_id, "members"] as const,
+  teams: (project_id?: string) => ["project", project_id, "teams"] as const,
+  userProjects: (user_id?: string) => ["user", user_id, "projects"] as const,
+  membership: (project_id?: string, user_id?: string) =>
+    [...projectKeys.userProjects(user_id), project_id] as const,
+  teamProjects: (team_id?: string) => ["team", team_id, "projects"] as const,
+};
+
+export const teamKeys = {
+  list: () => ["teams"] as const,
+  team: (team_id?: string) => ["team", team_id] as const,
+  members: (team_id?: string) => ["team", team_id, "members"] as const,
+  userTeams: (user_id?: string) => ["user", user_id, "teams"] as const,
+  membership: (team_id?: string, user_id?: string) =>
+    [...teamKeys.userTeams(user_id), team_id] as const,
+};
