e93168dbf7e34599ae5ac253747cbb4a7cf64e95
Mostafa F <mostafa.foruji@gmail.com>
2024-09-20 18:45:36 +0330

    WIP

---
+++ packages/ui/src/components/submit-button.tsx
@@ -0,0 +1,28 @@
+import { Loader2 } from "lucide-react";
+import { cn } from "../utils";
+import { Button, type ButtonProps } from "./button";
+
+export function SubmitButton({
+  children,
+  isSubmitting,
+  ...props
+}: {
+  children: React.ReactNode;
+  isSubmitting: boolean;
+} & ButtonProps) {
+  return (
+    <Button
+      disabled={isSubmitting}
+      {...props}
+      className={cn(props.className, "relative")}
+    >
+      <span className={cn({ "opacity-0": isSubmitting })}>{children}</span>
+
+      {isSubmitting && (
+        <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+          <Loader2 className="h-4 w-4 animate-spin" />
+        </span>
+      )}
+    </Button>
+  );
+}
